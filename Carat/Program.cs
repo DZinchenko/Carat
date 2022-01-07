a40b0d3225364d1deb00078fa3e10f5ee1e47f79
Danylo Zinchenko <zindan466@gmail.com>
2022-01-07 21:15:40 +0200

    Enchanced bad db file exception handling

--- Carat/Program.cs
+++ Carat/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Microsoft.Data.Sqlite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,18 +18,39 @@ namespace Carat
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            try
+
+            Application.ThreadException += new ThreadExceptionEventHandler(Application_ThreadException);
+            Application.SetUnhandledExceptionMode(UnhandledExceptionMode.CatchException);
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(CurrentDomain_UnhandledException);
+
+            Application.Run(new MainForm());
+        }
+
+        static void Application_ThreadException(object sender, ThreadExceptionEventArgs e)
+        {
+            if (e.Exception.Message.Contains("no such column") || e.Exception.Message.Contains("no such table"))
             {
-                Application.Run(new MainForm());
+                MessageBox.Show("Ви використовуєте стару версію бази даних!");
             }
-            catch(Exception ex)
+            else
             {
-                if(ex.Message.Contains("no such column") || ex.Message.Contains("no such table"))
-                {
-                    MessageBox.Show("Ви використовуєте стару версію бази даних!");
-                    Application.Run(new MainForm());
-                }
+                MessageBox.Show(e.Exception.Message, "Unhandled Thread Exception");
             }
+            Application.Run();
+        }
+
+        static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            var message = (e.ExceptionObject as Exception).Message;
+            if (message.Contains("no such column") || message.Contains("no such table"))
+            {
+                MessageBox.Show("Ви використовуєте стару версію бази даних!");
+            }
+            else
+            {
+                MessageBox.Show(message, "Unhandled UI Exception");
+            }
+            Application.Run();
         }
     }
 }
